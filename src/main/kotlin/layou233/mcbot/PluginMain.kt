ca87a128358a012abae71af31bfe731b3b28c903
layou233 <1665571328@qq.com>
2021-02-19 19:24:07 +0800

    Fix QQ Bounding Function

--- src/main/kotlin/layou233/mcbot/PluginMain.kt
+++ src/main/kotlin/layou233/mcbot/PluginMain.kt
@@ -11,6 +11,7 @@ import net.mamoe.mirai.event.events.*
 import net.mamoe.mirai.message.data.MessageSource.Key.quote
 import net.mamoe.mirai.message.data.buildMessageChain
 import net.mamoe.mirai.utils.info
+import java.io.IOException
 import java.util.Locale.ROOT
 
 object PluginMain : KotlinPlugin(
@@ -22,75 +23,89 @@ object PluginMain : KotlinPlugin(
     override fun onEnable() {
         logger.info { "MCBot Plugin loaded" }
         GlobalEventChannel.subscribeAlways<Event> {
-            when (this) {
-                is GroupMessageEvent -> {
+            try {
+                when (this) {
+                    is GroupMessageEvent -> {
 //                    val whom: String = this.sender.toString().substring(7, (this.sender.toString().length - 1))
-                    val saying = this.message.contentToString().replace("\\s".toRegex(), "")
+                        val saying = this.message.contentToString().replace("\\s", "")
 //                    logger.info { saying }
-                    if (saying.length > 1) {
-                        if (saying.substring(0, 2) == "sb") {
-                            if (saying.substring(2, 7) == "money") {
-                                val playerId: String = saying.substring(7)
-                                when (val money: Int = sb_money(playerId)) {
-                                    -1 -> this.group.sendMessage(buildMessageChain {
+                        if (saying.length > 1) {
+                            if (saying.substring(0, 2) == "sb") {
+                                if (saying.substring(2, 7) == "money") {
+                                    val playerId: String = saying.substring(7)
+                                    when (val money: Int = sb_money(playerId)) {
+                                        -1 -> this.group.sendMessage(buildMessageChain {
+                                            +this@subscribeAlways.message.quote()
+                                            +"$playerId 没有Skyblock存档！"
+                                        })
+                                        -2 -> this.group.sendMessage(buildMessageChain {
+                                            +this@subscribeAlways.message.quote()
+                                            +"玩家 $playerId 无Hypixel数据或不存在！"
+                                        })
+                                        -3 -> this.group.sendMessage(buildMessageChain {
+                                            +this@subscribeAlways.message.quote()
+                                            +"网络超时，请重试."
+                                        })
+                                        else -> this.group.sendMessage(buildMessageChain {
+                                            +this@subscribeAlways.message.quote()
+                                            +"$playerId 共拥有 $money coins"
+                                        })
+                                    }
+                                }
+                            }
+                        }
+                        if (saying.length > 4) {
+                            if (saying.toLowerCase(ROOT).substring(0, 2) == "查q") {
+                                val qqNumber: String = saying.substring(2)
+                                this.group.sendMessage(buildMessageChain {
+                                    +this@subscribeAlways.message.quote()
+                                    +"查询 $qqNumber 结果：\n手机绑定：${getQBounding(qqNumber)} "
+                                })
+                            }
+                            else if (saying.substring(0, 4) == "skin") {
+                                val playerId: String = saying.substring(4)
+                                val skinAddr: String? = skin(playerId)
+                                if (skinAddr == null) this.group.sendMessage(buildMessageChain {
+                                    +this@subscribeAlways.message.quote()
+                                    +"未找到该玩家或服务器出错"
+                                })
+                                else {
+                                    this.group.sendMessage(buildMessageChain {
                                         +this@subscribeAlways.message.quote()
-                                        +"$playerId 没有Skyblock存档！"
-                                    })
-                                    -2 -> this.group.sendMessage(buildMessageChain {
-                                        +this@subscribeAlways.message.quote()
-                                        +"玩家 $playerId 无Hypixel数据或不存在！"
-                                    })
-                                    -3 -> this.group.sendMessage(buildMessageChain {
-                                        +this@subscribeAlways.message.quote()
-                                        +"网络超时，请重试."
-                                    })
-                                    else -> this.group.sendMessage(buildMessageChain {
-                                        +this@subscribeAlways.message.quote()
-                                        +"$playerId 共拥有 $money coins"
+                                        +skinAddr
                                     })
                                 }
                             }
                         }
+                        if (saying.toLowerCase(ROOT) == "status") this.group.sendMessage(buildMessageChain {
+                            +this@subscribeAlways.message.quote()
+                            +" 机器人状态：运行中……"
+                        })
                     }
-                    if (saying.length > 2) {
-                        if (saying.toLowerCase(ROOT).substring(0, 2) == "查q") {
-                            val qqNumber: String = saying.substring(2)
-                            this.group.sendMessage(buildMessageChain {
-                                +this@subscribeAlways.message.quote()
-                                +"查询 $qqNumber 结果：\n手机绑定：${getQBounding(qqNumber)}"
-                            })
-                        }
-                    }
-                    if (saying.length > 4) {
-                        if (saying.substring(0, 4) == "skin") {
-                            val playerId: String = saying.substring(4)
-                            val skinAddr: String? = skin(playerId)
-                            if (skinAddr == null) this.group.sendMessage(buildMessageChain {
-                                +this@subscribeAlways.message.quote()
-                                +"未找到该玩家或服务器出错"
-                            })
-                            else {
-                                this.group.sendMessage(buildMessageChain {
-                                    +this@subscribeAlways.message.quote()
-                                    +skinAddr
+                    is FriendMessageEvent -> {
+                        val saying = this.message.contentToString().replace("\\s", "")
+                        if (saying.length > 2) {
+                            if (saying.toLowerCase(ROOT).substring(0, 2) == "chaq") {
+                                val qqNumber: String = saying.substring(2)
+                                this.sender.sendMessage(buildMessageChain {
+                                    "查询 $qqNumber 结果：\n手机绑定：${
+                                        getQBounding(qqNumber)
+                                    }"
                                 })
                             }
                         }
+                        if (saying.toLowerCase(ROOT) == "status") this.sender.sendMessage("机器人状态：运行中……")
                     }
+                    is GroupTempMessageEvent -> this.sender.sendMessage("为防止部分离奇问题出现，请先添加好友再使用功能！")
                 }
-                is FriendMessageEvent -> {
-                    val saying = this.message.contentToString().replace("\\s".toRegex(), "")
-                    if (saying.length > 2) {
-                        if (saying.toLowerCase(ROOT).substring(0, 2) == "查q") {
-                            val qqNumber: String = saying.substring(2)
-                            this.sender.sendMessage(buildMessageChain { "查询 $qqNumber 结果：\n手机绑定：${getQBounding(qqNumber)}" })
-                        }
-                    }
+            } catch (e: IOException) {
+                when (this) {
+                    is GroupMessageEvent -> this.group.sendMessage(buildMessageChain {
+                        +this@subscribeAlways.message.quote()
+                        +" 网络请求出现故障，请重试？"
+                    })
+                    is FriendMessageEvent -> this.sender.sendMessage("网络请求出现故障，请重试？")
                 }
-                is GroupTempMessageEvent -> this.sender.sendMessage(buildMessageChain {
-                    +this@subscribeAlways.message.quote()
-                    +"为防止部分离奇问题出现，请先添加好友再使用功能！"
-                })
             }
         }
     }
--- src/main/kotlin/layou233/mcbot/qbounds/qBounds.kt
+++ src/main/kotlin/layou233/mcbot/qbounds/qBounds.kt
@@ -2,9 +2,11 @@ package layou233.mcbot.qbounds
 
 import layou233.mcbot.httpClient.client
 import okhttp3.Request
+import java.io.IOException
 
 fun getQBounding(qqNumber: String): String {
-    val back = client.newCall(Request.Builder().url("https://api.smokeclient.cn/qb-api/?qq=$qqNumber").build())
+    val back: String = client.newCall(Request.Builder().url("https://api.smokeclient.cn/qb-api/?qq=$qqNumber").build())
         .execute().body!!.string()
-    return back.substring(back.indexOf("mobile") + 9, back.length - 2).replace("ul","|无数据或出错| ")
+    val mobile: String = Regex("mobile\": \"([0-9]*\")|(null)").find(back)?.value ?: throw IOException("Bad Response")
+    return mobile.substring(10, mobile.length - 1).replace("ul", "|无数据或出错|")
 }
